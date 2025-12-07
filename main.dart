cbc13b16207cb2faa4ee248b5eb4782275ae57ec
Dhan5hg <156923270+Dhan5hg@users.noreply.github.com>
2025-12-07 23:11:41 +0100

    Initialize Flutter app with LoginPage
    Set up main application structure with login page.

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:assignment_1/LoginPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Auth Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(), // Set login page as home
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
